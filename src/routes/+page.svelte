9337e65742d67d2cf369dae00fa6df4e06af1918
Joshua Coombes-Hall <jtec77@gmail.com>
2024-04-09 16:28:46 +1000

    Testing Cloudflare Environment Variables.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import { TEST_ENV_VAR } from '$env/static/private';
+</script>
+
+<h1>Actor Person: {TEST_ENV_VAR}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
